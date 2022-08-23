ea0a836ff84db950e39f15eb2ffde8d3bc224658
mansi4444 <100615059+mansi4444@users.noreply.github.com>
2022-08-23 14:23:42 +0530

    Update solar.cpp

--- solar.cpp
+++ solar.cpp
@@ -1,5 +1,7 @@
 #include <GL/glut.h>
 #include<math.h>
+#include<update.h>
+
 #define PI 3.14
 float angleMercury,angleVenus,angleEarth,angleMars,angleJupiter,angleSaturn,angleUranus,angleNeptune=0.0;
 GLfloat sx=0.2,sy=0.2,sz=0.2;
@@ -175,46 +177,6 @@ glPopMatrix();
 
 
 
-//inner planets will have more speed than outer
-void update(int value)
-{
-	
-angleMercury+=3;            ///if we decrease 3 to 2,,,planet will move slower
-if(angleMercury>360){
-    angleMercury-=360;}
-    
-angleVenus+=2;
-if(angleVenus>360){
-    angleVenus-=360;}          ///angle-360 done so that after one circle it becomes angle again... 
-
-angleEarth+=1.7;
-if(angleEarth>360){
-    angleEarth-=360;}
-
-angleMars+=1.5;
-if(angleMars>360){
-    angleMars-=360;}
-
-angleJupiter+=1;
-if(angleJupiter>360){
-    angleJupiter-=360;}
-
-angleSaturn+=0.5;
-if(angleSaturn>360){
-    angleSaturn-=360;}
-    
-angleUranus+=0.45;
-if(angleUranus>360){
-    angleUranus-=360;}
-
-angleNeptune+=0.3;
-if(angleNeptune>360){
-    angleNeptune-=360;}
-
-
-glutPostRedisplay();      
-glutTimerFunc(20,update,0);    // (no.ofmilliseconds,callbackfunction,value)....if value comes,we will stop this fucntion to call update function
-}
 
 
 
